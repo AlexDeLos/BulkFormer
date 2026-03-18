c48aa9cda80022ab91ddd2b5663b3a28536fa7dc
Alex <64327268+AlexDeLos@users.noreply.github.com>
2026-03-18 12:22:56 +0100

    Update build_graph_ara.py

--- utils/build_graph_ara.py
+++ utils/build_graph_ara.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 import torch
 
-EXPR_PATH  = '/home/alex/Documents/GitHub/Dataset_fusion_Microarray/new_storage/final_data/RMA_Microarray_Combined.csv'
+# EXPR_PATH  = '/home/alex/Documents/GitHub/Dataset_fusion_Microarray/new_storage/final_data/RMA_Microarray_Combined.csv'
+EXPR_PATH  = '/tudelft.net/staff-umbrella/GeneExpressionStorage/final_data/imputed.csv'
 GENE_INFO  = 'metadata/arabidopsis_gene_info.csv'
 
 gene_info = pd.read_csv(GENE_INFO)
@@ -36,6 +37,6 @@ for i in range(G):
 edge_index  = torch.tensor([rows, cols], dtype=torch.long)
 edge_weight = torch.tensor(vals, dtype=torch.float32)
 
-torch.save(edge_index,  'data/G_ath.pt')
-torch.save(edge_weight, 'data/G_ath_weight.pt')
+torch.save(edge_index,  '/tudelft.net/staff-umbrella/GeneExpressionStorage/final_data/graph_data/G_ath.pt')
+torch.save(edge_weight, '/tudelft.net/staff-umbrella/GeneExpressionStorage/final_data/graph_data/G_ath_weight.pt')
 print(f'Graph: {len(vals)} edges across {G} genes')
